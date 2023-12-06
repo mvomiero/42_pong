69a8e1102a2482acae5ab96019f1204b91c69770
Marco Vomiero <mvomiero@student.42berlin.de>
2023-12-06 12:45:09 +0100

    setting for game local

--- pong/urls.py
+++ pong/urls.py
@@ -1,11 +1,14 @@
 # pong/urls.py
 
 from django.urls import path, include
-from pong.views import index, pong, dashboard, room
+from pong.views import index, pong, dashboard, room, play_local, play_remote, pong_local
 
 urlpatterns = [
 	path('', index, name='index'),
 	path('room/', room, name='room'),
+    path('play_local/', play_local, name='play_local'),
+    path('play_remote/', play_remote, name='play_remote'),
+    path('pong_local/', pong_local, name='pong_local'),
     # the value <> is then passsed to the view as a parameter
     # and corresponds to all between play/ and ? in the url
     path('play/<room_code>', pong),
--- pong/views.py
+++ pong/views.py
@@ -23,5 +23,28 @@ def pong(request, room_code):
     }
     return render(request, "pong/pong.html", context)
 
+
+def pong_local(request):
+    player1 = request.GET.get("player1") # get the choice from the url
+    player2 = request.GET.get("player2") # get the choice from the url
+    context = {
+        "player1": player1,
+        "player2": player2,
+    }
+    return render(request, "pong/pong.html", context)
+
 def dashboard(request):
 	return render(request, "pong/dashboard.html")
+
+def play_local(request):
+    if request.method == "POST":
+        player1 = request.POST.get("player1")
+        player2 = request.POST.get("player2")
+        return redirect(
+            '/pong_local/?&player1=%s&player2=%s' 
+            %(player1, player2) # replacing the strings just like printf
+        )
+    return render(request, "pong/play_local.html")
+
+def play_remote(request):
+    return render(request, "pong/play_remote.html")
