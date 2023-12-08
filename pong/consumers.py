11f231f69ade81036c6e0d2bd554222c1671bcf0
flo-12 <flobecht@hotmail.com>
2023-12-08 19:11:57 +0100

    [backend] remote game working (adding & deleting with different rooms), local game with error, first concepts of tournament implemented (at the moment switched off)

--- pong/consumers.py
+++ pong/consumers.py
@@ -20,69 +20,64 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 # check at connect that we don't have duplicates in alias (player name)
 class PongConsumer(AsyncJsonWebsocketConsumer):
 
-    # player:flo    | match:semi1
-    # player:marco  | match:semi1
-    # player:pierre | match:semi2
-    # player:john   | match:semi2
     connected_users = {}  # Dictionary to store connected users and their connections
-            # connected_users = {user_id: {channel_name, group_name, match_id}, user_id: {...}, ...}
-    set_match = {}  # Dictionary to store matches and their players (NEW)
-            # set_match = {match_id: [player, player], match_id: [player, player], ...}
+            # connected_users = {user_id: (channel_name, group_name, match_id), user_id: (...), ...}
+    set_matches = {}  # Dictionary to store matches and their players
+            # OLD: set_matches = {match_id: [player, player], match_id: [player, player], ...}
+            # NEW (has to be implemented):
+                # set_matches = {
+                    # 'id1': {
+                    #     'players':    ['player1', 'player2'],
+                    #     'tournament': ['tournament_id']
+                    # },
+                    # 'id2': {
+                    #     'players':    ['player3', 'player4'],
+                    #     'tournament': ['None']        # "None" if not part of a tournament
+                    # },
 
+    set_tournaments = {}  # Dictionary to store tournaments and their matches
+                # set_tournaments = {
+                    # 'id1': {
+                    #     'players': ['player1', 'player2'],
+                    #     'matches': ['match1', 'match2']
+                    # },
+                    # 'id2': {
+                    #     'players': ['player3', 'player4'],
+                    #     'matches': ['match2', 'match3']
+                    # },
+        # add a player:     set_tournaments['id1']['players'].append('player5')
+        # remove player:    if player_to_remove in my_dict[id_to_modify]['players']:
+        #                       set_tournaments['id1']['players'].remove('player5')
+        # retrieving all players: players = set_tournaments['id1']['players']
+        # clearing all players: set_tournaments['id1']['players'].clear()
+
+
+    # ************************************************************ #
+    # *********** ESTABLISH NEW WEBSOCKET CONNECTION ************* #
+    # ************************************************************ #
     async def connect(self):
         self.room_code = self.scope['url_route']['kwargs']['game_mode']
         self.player = self.scope['url_route']['kwargs']['player']
+        self.room_group_name = None
+
         print(f"room_code: {self.room_code}")
         print(f"player: {self.player}")
-        self.match_id = None  # NEW
-        self.tournament_id = None  # NEW
-        self.room_group_name = None  # NEW
         
-        # To Do: Handle douplicates in alias (player name) for matches
+        # Reject connection if player already exists
+        if self.player in self.connected_users:
+            print(f"Player {self.player} already exists.")
+            self.reject_connection(500)
+            return
 
         # Add the player to a match
         if self.room_code == "match":
-            
-            # Find the first match ID with only one player
-            open_match = None
-            for match_id, players in self.set_match.items():
-                print(f"match_id: {match_id} | players: {players} | len(players): {len(players)}")
-                if len(players) == 1:
-                    open_match = match_id
-                    break
-            
-            # Refuse if set_match is full
-            nbr_matches = len(self.set_match)
-            print(f"len(matches): {nbr_matches}")
-            if open_match is None and len(self.set_match) >= 1:
-                print("Match is full.")
-                await self.close(code=507)
+            if not await self.add_player_to_match():
                 return
-            
-            print(f"open_match: {open_match}")
-            if open_match is None:  # create a new match
-                # Find the smallest available ID for the new match
-                new_match_id = 1
-                while new_match_id in self.set_match:
-                    new_match_id += 1
-                self.set_match[new_match_id] = set()
-                self.set_match[new_match_id].add(self.player)
-                self.match_id = new_match_id
-                
-            else:
-                self.set_match[open_match].add(self.player)
-                self.match_id = open_match
-
-            print(f"self.set_match: {self.set_match}")
-
-            self.room_group_name = f'{self.room_code}_{self.match_id}'  # generate room name
-            print(f"room_group_name: {self.room_group_name}")
-
-            # Add player to the dictionary
-            # (To Do: check if player already exists)
-            self.connected_users[self.player] = set()
-            user_info_tuple = (self.channel_name, self.room_group_name, self.match_id)
-            self.connected_users[self.player].add(user_info_tuple)
+        elif self.room_code == "tournament":
+            print("Tournament mode is not implemented yet.")
+            if not await self.add_player_to_tournament():
+                self.reject_connection(501)
+                return
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -99,12 +94,120 @@ class PongConsumer(AsyncJsonWebsocketConsumer):
         group_size = len(self.channel_layer.groups.get(self.room_group_name, {}).items())
         print(f"The size of group '{self.room_group_name}' is: {group_size}")
 
-        # Additional print statement at connection
-        # if user_id:
-        #     print(f"User {user_id} connected with channel name {self.channel_name}")
-
         await self.accept()
 
+
+    # ************************************************************ #
+    # ***************** LOGIC PLAYER MATCHMAKING ***************** #
+    # ************************************************************ #
+    async def add_player_to_match(self):
+        self.match_id = None
+
+        max_nbr_games = 1000   # important to avoid overflow and undefined behavior with set_matcheses
+
+        # Find the first match ID with only one player
+        open_match = None
+        for id, players in self.set_matches.items():
+            print(f"match_id: {id} | players: {players} | len(players): {len(players)}")
+            if len(players) == 1:
+                open_match = id
+                break
+        
+        # Reject if set_matches is full
+        if open_match is None and len(self.set_matches) >= max_nbr_games:
+            print("set_match is full.")
+            self.reject_connection(507)
+            return False
+            
+        print(f"open_match: {open_match}")
+        if open_match is None:  # create a new match
+            # Find the smallest available ID for the new match
+            new_match_id = 0
+            while new_match_id in self.set_matches:
+                new_match_id += 1
+            self.set_matches[new_match_id] = set()
+            self.set_matches[new_match_id].add(self.player)
+            self.match_id = new_match_id
+        else:                   # add player to existing match
+            self.set_matches[open_match].add(self.player)
+            self.match_id = open_match
+
+        print(f"self.set_matches: {self.set_matches}")
+
+        self.room_group_name = f'{self.room_code}_{self.match_id}'  # generate room name
+        print(f"room_group_name: {self.room_group_name}")
+
+        # Add player to the dictionary
+        self.connected_users[self.player] = set()
+        user_info_tuple = (self.channel_name, self.room_group_name, self.match_id)
+        self.connected_users[self.player].add(user_info_tuple)
+
+        return True
+
+
+    # ************************************************************ #
+    # *************** LOGIC TOURNAMENT MATCHMAKING *************** #
+    # ************************************************************ #
+    async def add_player_to_tournament(self):
+        # set_tournaments = {
+                    # 'id1': {
+                    #     'players': ['player1', 'player2'],
+                    #     'matches': ['match1', 'match2']
+                    # },
+                    # 'id2': {
+                    #     'players': ['player3', 'player4'],
+                    #     'matches': ['match2', 'match3']
+                    # },
+        # add a player:     set_tournaments['id1']['players'].append('player5')
+        # remove player:    if player_to_remove in my_dict[id_to_modify]['players']:
+        #                       set_tournaments['id1']['players'].remove('player5')
+        # retrieving all players: players = set_tournaments['id1']['players']
+        # clearing all players: set_tournaments['id1']['players'].clear()
+
+        self.tournament_id = None
+        max_nbr_tournaments = 0   # important to avoid overflow and undefined behavior with set_tournamentses
+        players_per_tournament = 4
+
+        # Find the first tournament ID with less than (players_per_tournament) players
+        open_tournament = None
+        for id, data in self.set_tournaments.items():
+            print(f"tournament_id: {id} | players: {data['players']} | len(players): {len(data['players'])}")
+            if len(data['players']) < players_per_tournament:
+                open_tournament = id
+                break
+        
+        # Reject if set_tournaments is full
+        if open_tournament is None and len(self.set_tournaments) >= max_nbr_tournaments:
+            print("set_tournament is full.")
+            self.reject_connection(507)
+            return False
+
+        print(f"open_tournament: {open_tournament}")
+        id = open_tournament
+        if id is None:  # create a new tournament
+            # Find the smallest available ID for the new tournament
+            id = 0
+            while id in self.set_tournaments:
+                id += 1
+            self.set_tournaments[id] = set()
+        self.set_tournaments[id].add(self.player)
+        self.tournament_id = id
+        
+
+        return True
+
+
+    # ************************************************************ #
+    # ********************* REJECT WEBSOCKET ********************* #
+    # ************************************************************ #
+    async def reject_connection(self, code):
+        await self.accept()
+        await self.close(code=code)
+
+
+    # ************************************************************ #
+    # ******************* DISCONNECT WEBSOCKET ******************* #
+    # ************************************************************ #
     async def disconnect(self, close_code):
         # Remove the connection from the dictionaries when a user disconnects
         if self.room_group_name is None:
@@ -119,17 +222,17 @@ class PongConsumer(AsyncJsonWebsocketConsumer):
             for user_info_tuple in self.connected_users[user_id]:
                 match_id = user_info_tuple[2]  # Assuming match_id is the third element
             print(f"Retrieved match_id {match_id} for user_id {user_id}.")
-            if match_id and match_id in self.set_match and user_id in self.set_match[match_id]:
-                self.set_match[match_id].discard(user_id)
-                if not self.set_match[match_id]:
-                    del self.set_match[match_id]
+            if match_id and match_id in self.set_matches and user_id in self.set_matches[match_id]:
+                self.set_matches[match_id].discard(user_id)
+                if not self.set_matches[match_id]:
+                    del self.set_matches[match_id]
             
             # Clear and delete user from connected_users
             self.connected_users[user_id].clear()
             if not self.connected_users[user_id]:
                 del self.connected_users[user_id]
 
-            print(f"self.set_match: {self.set_match}")
+            print(f"self.set_matches: {self.set_matches}")
             print(f"self.connected_users: {self.connected_users}")
 
             # self.connected_users[user_id].discard(self.channel_name)
