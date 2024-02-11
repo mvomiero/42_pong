d47c3c36cef7c35905b442c943c513bef5419e00
Graham Booth <gbooth@c4c6c2.42berlin.de>
2024-02-11 11:32:44 +0100

    fixed the issue  by checking if admin account exists before calling createsuperuser in entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# Check if superuser already exists
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists())" | python manage.py shell | grep -q "False"
+
+# If superuser does not exist, create it
+if [ $? -eq 0 ]; then
+    python manage.py createsuperuser --noinput
+else
+    echo "Superuser exists, skipping 'createsuperuser' command"
+fi
+
 python manage.py runserver 127.0.0.1:8000 &
 nginx -g 'daemon off;'
